829001a2c7fae48c08a94872b7c21e0e4f5db7e1
MuriloVicente <murilovicent@gmail.com>
2023-09-19 16:12:22 -0300

    commit

---
+++ src/components/starwars/Personagens.tsx
@@ -0,0 +1,21 @@
+export default function Personagens() {
+    async function simularChamadaAPI() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(1)
+            }, 6000)
+        })
+    }
+
+    async function obterPersonagens() {
+        console.log('Inicio da chamada')
+        await simularChamadaAPI()
+        console.log('Fim da chamada')
+    }
+    return (
+        <div>
+            <h1>Personagens</h1>
+            <button onClick={obterPersonagens} className="bg-blue-500 p-2">Obter</button>
+        </div>
+    )
+}
